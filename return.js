0b9d3e5dc00115817bb2141bdb72da89ff5a15a3
sadiaprova12 <sadiaprova51717@gmail.com>
2024-05-04 00:10:35 +0600

    function return and set return value to a variable

---
+++ return.js
@@ -0,0 +1,18 @@
+//Function Return And Set Return Value To A Variable
+
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf(number) {
+    const half = number / 2;
+    return half;
+}
+
+tenTimes(5);
+
+const output = tenTimes(5);
+console.log('output', output);
+
+const bigNumber = tenTimes(99);
